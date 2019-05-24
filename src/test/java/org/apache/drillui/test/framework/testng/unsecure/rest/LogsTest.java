a7bda4c2945f846d137eb2447a0fc9a1f5b1e0ea
Pavel Semenov <semenovpavel1987@gmail.com>
2019-05-24 16:10:32 +0300

    Five tests for /log/{log_name}/content.json endpoint

--- src/test/java/org/apache/drillui/test/framework/testng/unsecure/rest/LogsTest.java
+++ src/test/java/org/apache/drillui/test/framework/testng/unsecure/rest/LogsTest.java
@@ -15,21 +15,18 @@
  * limitations under the License.
  */
 package org.apache.drillui.test.framework.testng.unsecure.rest;
-
 import org.testng.annotations.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasItems;
 
 public class LogsTest extends BaseRestTest {
   //todo: what if sqlline.log file is not yet created?
-  //@Test
+  @Test
   public void checkLogsPage() {
     SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
     given()
@@ -41,8 +38,62 @@ public class LogsTest extends BaseRestTest {
             equalTo("drillbit.log"),
             equalTo("drillbit.out"),
             equalTo("drillbit_queries.json"),
-            equalTo("sqlline.log"))
-        )
+            equalTo("sqlline.log")))
         .body("lastModified", hasItem(containsString(df.format(new Date()))));
+  }
+  @Test
+  public void DrillbitLogContent(){
+    given()
+            .when()
+            .get("/log/drillbit.log/content.json")
+            .then()
+            .statusCode(200)
+            .body("name",equalTo("drillbit.log"))
+            .body(containsString("lines"))
+            .body("maxLines", equalTo(10000));
+  }
+  @Test
+  public void DrillbitOutContent(){
+      given()
+            .when()
+            .get("/log/drillbit.out/content.json")
+            .then()
+            .statusCode(200)
+            .body("name",equalTo("drillbit.out"))
+            .body(containsString("lines"))
+            .body("maxLines", equalTo(10000));
+  }
+  @Test
+  public void DrillbitQueriesContent(){
+      given()
+            .when()
+            .get("/log/drillbit_queries.json/content.json")
+            .then()
+            .statusCode(200)
+            .body("name",equalTo("drillbit_queries.json"))
+            .body(containsString("lines"))
+            .body("maxLines", equalTo(10000));
+  }
+  @Test
+  public void SqllineContent(){
+      given()
+            .when()
+            .get("/log/sqlline.log/content.json")
+            .then()
+            .statusCode(200)
+            .body("name",equalTo("sqlline.log"))
+            .body(containsString("lines"))
+            .body("maxLines", equalTo(10000));
+  }
+  @Test
+  public void SqllineQueriesContent(){
+      given()
+            .when()
+            .get("/log/sqlline_queries.json/content.json")
+            .then()
+            .statusCode(200)
+            .body("name",equalTo("sqlline_queries.json"))
+            .body(containsString("lines"))
+            .body("maxLines", equalTo(10000));
   }
 }
